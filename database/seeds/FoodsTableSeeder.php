3494e5e0ac0417e98bb5776a576be36b9dbb3976
Keeley Peck <peck.keeley@gmail.com>
2016-12-03 18:07:35 -0500

    all tables seeded

--- database/seeds/FoodsTableSeeder.php
+++ database/seeds/FoodsTableSeeder.php
@@ -29,17 +29,5 @@ class FoodsTableSeeder extends Seeder
         'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
         'food_name' => 'Orange',
         ]);
-        
-        $existingFoods = Food::all()->keyBy('food_name')->toArray();
-        foreach($foods as $food) {
-            # If the food does not already exist, add them
-            if(!array_key_exists(food[0],$existingFoods)) {
-                $food = User::create([
-                    'created_at' => $food[0],
-                    'updated_at' => $food[1],
-                    'food_name' => $food[2],
-                ]);
-            }
-        }
-    }
+}
 }
